f1f4a1e3efea94ca16bb5852cf14384091e7672e
phongtran <phongtrantph@gmail.com>
2021-05-12 22:10:14 +0700

    first

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //
+    public function name($name){
+        return "hello abdajkbvfksdbk ".$name;
+//         echo "# bai_test" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/phongtran2320/bai_test.git
+// git push -u origin main
+    }
+
+    public function a(){
+        return "heheheh ";
+    }
+    public function c(){
+        return "hahahahahah ";
+    }
+    public function b($name){
+        return \Redirect::route('test', $name);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\TestController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello/{name}',function($name){
+    return "hello ".$name;
+});
+
+Route::get('test/{name}','TestController@name')->name('test');
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('g1','TestController@a')->name('a');
+
+    Route::get('g2/{name}','TestController@b')->name('b');
+
+    Route::get('g3','TestController@c')->name('c');
+});
